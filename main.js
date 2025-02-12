822dd6357220938e1be3520ae081a3e7fc19aea4
GabrielDev-Junior <gabrielavelinoamorim@gmail.com>
2025-02-11 23:57:41 -0300

    Commit - exercicio_jquery

---
+++ main.js
@@ -0,0 +1,32 @@
+$('header #addForm').click(function () {
+  $('#formulario').slideDown();
+})
+
+$('header #addForm').dblclick(function () {
+  $('#formulario').slideUp();
+})
+
+$('#formulario').on('submit', function (e) {
+  e.preventDefault();
+})
+
+$('#enviar').click(function () {
+
+  const tarefaInserida = $('#tarefaInserida').val();
+
+  if (tarefaInserida !== '') {
+    let novoItem = $('<li></li>');
+
+    $(`<div class="tarefas">
+      <p id="paragrafo">${tarefaInserida}</p>
+      <input type="checkbox">
+    </div>`).appendTo(novoItem);
+    novoItem.appendTo('ul');
+    $('ul').css('display', 'block');
+    
+    $('#tarefaInserida').val('');
+  }
+  else {
+    alert("Informe uma tarefa!");
+  }
+})
